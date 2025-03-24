ec6ed3926a2023d613418c795e583fd4543010bc
hariharanCrayond <hariharan@crayond.co>
2025-03-25 00:25:30 +0530

    feat(root): commit added

---
+++ .commitlintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/** @type {import('cz-git').UserConfig} */
+
+const fs = require('node:fs');
+const path = require('node:path');
+const apps = fs.readdirSync(path.resolve(__dirname, 'src'));
+
+module.exports = {
+  rules: {},
+
+  prompt: {
+    useEmoji: true,
+    markBreakingChangeMode: false,
+    scopes: ['root', ...apps],
+    types: [
+      { value: 'feat', name: 'feat: A new feature' },
+      { value: 'fix', name: 'fix: A bug fix' },
+      { value: 'docs', name: 'docs: Documentation only changes' },
+      {
+        value: 'refactor',
+        name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+      },
+      { value: 'perf', name: 'perf: A code change that improves performance' },
+      {
+        value: 'test',
+        name: 'test: Adding missing tests or correcting existing tests',
+      },
+    ],
+  },
+};
